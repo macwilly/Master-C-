c0f5f882c3e2dabf15bf4e3421efefb2c99140ef
Mackenzie Willard <Mackenzie.Willard@truetandem.com>
2025-06-18 16:53:03 -0400

    Prompts user for input
    Asks the user to enter some text, captures their input,
    and then echoes that input back to the console. This
    provides a basic interactive experience.

--- MyAdditionCalculator/MyAdditionCalculator/Program.cs
+++ MyAdditionCalculator/MyAdditionCalculator/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Prints what is inside the brackets
+Console.WriteLine("Enter Something!");
+
+// takes the user input and stores it
+string userInput = Console.ReadLine();
+Console.WriteLine("You entered " +  userInput);

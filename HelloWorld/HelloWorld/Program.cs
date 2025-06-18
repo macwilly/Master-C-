a059c8d9ade9cb5e6a60d531d6ac9a68b2dbd80d
Mackenzie Willard <Mackenzie.Willard@truetandem.com>
2025-06-18 16:37:52 -0400

    Adds variable assignment and output
    Demonstrates variable declaration, assignment, and output to the console.
    Illustrates overriding variable values and keeping the console open.

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -1,2 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, Will!");
+
+// Declare a variable
+string myFriendsName;
+string myOtherFriend = "Milkshake";
+
+// assign a value
+myFriendsName = "Otto";
+
+// use the variable
+Console.WriteLine(myFriendsName);
+
+// overriding the variable value
+myFriendsName = "Zozo";
+
+Console.WriteLine(myFriendsName);
+
+Console.WriteLine(myOtherFriend);
+
+// This will keep Console open
+Console.ReadKey();

7f15e59e67a83395f3e918e2904df306ae36abcf
Mackenzie Willard <Mackenzie.Willard@truetandem.com>
2025-06-19 15:21:22 -0400

    Calculates the sum of two user inputs
    Asks the user for two numbers, calculates their sum, and displays the result.
    
    Replaces the previous program that only displayed user input.

--- MyAdditionCalculator/MyAdditionCalculator/Program.cs
+++ MyAdditionCalculator/MyAdditionCalculator/Program.cs
@@ -1,7 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
 // Prints what is inside the brackets
-Console.WriteLine("Enter Something!");
+Console.WriteLine("Enter A Number!");
+
+int myFirstNumber = 0;
+int mySecondNumber = 0;
+int sumOfUserInput = 0;
 
 // takes the user input and stores it
 string userInput = Console.ReadLine();
-Console.WriteLine("You entered " +  userInput);
+//myNumber = int.Parse(Console.ReadLine());
+myFirstNumber = int.Parse(userInput);
+
+Console.WriteLine("Enter a second number");
+
+userInput = Console.ReadLine();
+mySecondNumber = int.Parse(userInput);
+
+sumOfUserInput = myFirstNumber + mySecondNumber;
+
+Console.WriteLine($"{myFirstNumber} plus {mySecondNumber} = {sumOfUserInput}");
+
+Console.ReadKey();

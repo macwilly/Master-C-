8d790acaef89302bbdf7ea53cc3a2311b904d9ee
Mackenzie Willard <Mackenzie.Willard@truetandem.com>
2025-06-20 17:22:55 -0400

    Adds prime number checker functionality
    Adds functionality to check if user inputted number is a prime.
    
    Takes user input, validates it and outputs to the console
    whether the entered number is a prime or not.

---
+++ PrimeNumberChecker/PrimeNumberChecker/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter a whole number");
+
+int userNumber = int.Parse(Console.ReadLine());
+
+int divisor = 1;
+bool isPrime = true;
+string isPrimeDisplay = "is not a Prime Number.";
+
+while (isPrime) 
+{
+    while (divisor < userNumber)
+    {
+
+        if ((userNumber % divisor) > 0)
+        {
+            isPrime = false;
+        }
+        divisor++;
+    }
+    break;
+}
+
+
+if (isPrime) { 
+    isPrimeDisplay = "is a prime number.";
+}
+
+Console.WriteLine($"Your number {userNumber} {isPrimeDisplay}");
+
+Console.ReadLine();
+

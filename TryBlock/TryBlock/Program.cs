be6de1c199c348063517d768b3591244098e5c60
Mackenzie Willard <Mackenzie.Willard@truetandem.com>
2025-06-25 16:31:09 -0400

    Adds user input validation
    Validates the user input to ensure it's a number.
    Prompts the user to enter valid input if the initial input is not a number to avoid errors.

---
+++ TryBlock/TryBlock/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Give a number");
+
+string userInput = Console.ReadLine();
+int num1;
+
+if (!int.TryParse(userInput, out num1)) 
+{
+    Console.WriteLine("Please Write a number, if not the answer will be 1");
+    int.TryParse(Console.ReadLine(), out num1 );
+}
+
+
+num1++;
+Console.WriteLine($"User entered {userInput} +1 = {num1}");
+Console.ReadKey();

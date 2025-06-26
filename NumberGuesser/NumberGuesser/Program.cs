b52dd40b974fa338dde939513773f00623c1e068
Mackenzie Willard <Mackenzie.Willard@truetandem.com>
2025-06-26 17:28:55 -0400

    Implements number guessing game
    Creates a number guessing game where the user tries to guess a hidden number between 1 and 100.
    
    The game provides feedback to the user whether their guess is too high or too low.
    It also keeps track of the number of guesses it takes to find the hidden number.

---
+++ NumberGuesser/NumberGuesser/Program.cs
@@ -0,0 +1,29 @@
+﻿Random random = new Random();
+int numberOfGuesses = 0;
+int startNumber = 1;
+int endNumber = 100;
+int hiddenNumber = random.Next(startNumber, endNumber);
+int userGuessedNumber = 0;
+
+Console.WriteLine($"Guess a number between {startNumber} and {endNumber}");
+numberOfGuesses++;
+int.TryParse(Console.ReadLine(), out userGuessedNumber);
+
+while (userGuessedNumber != hiddenNumber)
+{
+    if (userGuessedNumber > hiddenNumber)
+    {
+        Console.WriteLine("The number you guessed was higher than the hidden number. Guess again.");
+        numberOfGuesses++;
+        int.TryParse(Console.ReadLine(), out userGuessedNumber);
+    }
+    else
+    {
+        Console.WriteLine("The number you guessed was lower than the hidden number. Guess again.");
+        numberOfGuesses++;
+        int.TryParse(Console.ReadLine(), out userGuessedNumber);
+    }
+}
+
+Console.WriteLine($"You got it! The hidden number was {hiddenNumber}. It took you {numberOfGuesses} guesses.");
+Console.ReadLine();

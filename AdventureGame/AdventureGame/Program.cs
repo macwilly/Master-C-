3eee868dbecfb4d3e8fc6212a391a149cc56e7c3
Mackenzie Willard <Mackenzie.Willard@truetandem.com>
2025-06-26 18:37:34 -0400

    Introduces initial game loop
    Adds the first choice in the game, whether to enter the forest or camp.
    
    This introduces the first bit of gameplay after character creation.

--- AdventureGame/AdventureGame/Program.cs
+++ AdventureGame/AdventureGame/Program.cs
@@ -1,9 +1,9 @@
-﻿using System.Linq.Expressions;
-
-string characterName = "";
+﻿string characterName = "";
 string characterClass = "";
 int characterClassInt = 0;
+bool choseToEnterOrCamp = false;
 string classList = "\n 1) Warrior\n 2) Wizard\n 3) Archer";
+string forestOrCamp = "Do you enter the (f)orest or (c)amp out?";
 Console.WriteLine("Welcome to the Adventure Game!");
 Console.WriteLine("Enter your charater's name: ");
 characterName = Console.ReadLine();
@@ -39,12 +39,38 @@ while (characterClass == "")
             Console.WriteLine($"Please choose one of the class:{classList}");
             classInput = Console.ReadLine();
         }
-        else { 
+        else
+        {
             characterClass = classInput;
         }
     }
 }
 
-Console.WriteLine($"Welcome {characterName} you will make a fine {characterClass}");
+Console.WriteLine($"Welcome {characterName} you will make a fine {characterClass}.\n You find yourself at the edge of the Dark Forest.\n {forestOrCamp}");
+
+string enterForestOrCamp = Console.ReadLine();
+
+while (!choseToEnterOrCamp)
+{
+    string localForestOrCamp = enterForestOrCamp.ToLower().Trim();
+    if (localForestOrCamp != "f" && localForestOrCamp != "forest" && localForestOrCamp != "c" && localForestOrCamp != "camp" && localForestOrCamp != "camp out")
+    {
+        Console.WriteLine($"Brave {characterName} the {characterClass} that is not a valid option.\n {forestOrCamp}");
+        enterForestOrCamp = Console.ReadLine();
+    }
+    else
+    {
+        choseToEnterOrCamp = true;
+    }
+}
+
+if (enterForestOrCamp.ToLower().Trim() == "f" || enterForestOrCamp.ToLower().Trim() == "forest")
+{
+    Console.WriteLine("You boldly enter The Dark Forest. Not fearing what lies ahead!");
+}
+else 
+{
+    Console.WriteLine("You decided to camp out and wait fot the light of day."); 
+}
 
 Console.ReadKey();

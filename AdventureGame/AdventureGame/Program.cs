b8676189f9571461632a58cc44f1efd07656d66c
Mackenzie Willard <Mackenzie.Willard@truetandem.com>
2025-06-26 18:05:53 -0400

    Initializes the adventure game
    Sets up the basic game structure.
    Prompts the user for their character's name and class.
    Validates the class selection using both integer and string inputs.
    Greets the player with their chosen name and class.

---
+++ AdventureGame/AdventureGame/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Linq.Expressions;
+
+string characterName = "";
+string characterClass = "";
+int characterClassInt = 0;
+string classList = "\n 1) Warrior\n 2) Wizard\n 3) Archer";
+Console.WriteLine("Welcome to the Adventure Game!");
+Console.WriteLine("Enter your charater's name: ");
+characterName = Console.ReadLine();
+Console.WriteLine($"Choose your Class:{classList}");
+var classInput = Console.ReadLine();
+
+while (characterClass == "")
+{
+    if (int.TryParse(classInput, out characterClassInt))
+    {
+        switch (characterClassInt)
+        {
+            case 1:
+                characterClass = "Warrior";
+                break;
+            case 2:
+                characterClass = "Wizard";
+                break;
+            case 3:
+                characterClass = "Archer";
+                break;
+            default:
+                Console.WriteLine($"Please Choose one of the avaiable classes:{classList}");
+                classInput = Console.ReadLine();
+                break;
+        }
+    }
+    else
+    {
+        string localClass = classInput.ToLower().Trim();
+        if (localClass != "warrior" && localClass != "wizard" && localClass != "archer")
+        {
+            Console.WriteLine($"Please choose one of the class:{classList}");
+            classInput = Console.ReadLine();
+        }
+        else { 
+            characterClass = classInput;
+        }
+    }
+}
+
+Console.WriteLine($"Welcome {characterName} you will make a fine {characterClass}");
+
+Console.ReadKey();

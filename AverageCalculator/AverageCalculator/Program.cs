9715f6aaecdebd94923505b8b7670408deca735c
Mackenzie Willard <Mackenzie.Willard@truetandem.com>
2025-06-27 16:54:50 -0400

    Implements basic average calculator
    Creates a basic average calculator that prompts the user for scores,
    calculates the average, and handles "finished" input. It also clears
    the console after each calculation.

---
+++ AverageCalculator/AverageCalculator/Program.cs
@@ -0,0 +1,39 @@
+﻿int numberOfSubmittedScores = 0;
+int sumOfSubmittedScores = 0;
+bool finishedEnteringScores = false;
+bool end = false;
+double average = 0.0;
+
+do
+{
+    Console.WriteLine("Welcome to the average calculator. Please enter your scores one at a time.\n " +
+        "When you are finished type finished and we will return your average.");
+    do
+    {
+        int enteredScore = 0;
+        string userInput = Console.ReadLine();
+
+        if (userInput.ToLower().Trim() == "finished")
+        {
+            if (numberOfSubmittedScores > 0)
+            {
+
+            }
+            else
+            {
+                Console.WriteLine("Your average is 0. You did not enter any score.\n" +
+                    "Do you want to start over? Yes/No");
+
+            }
+
+        }
+        else if (int.TryParse(userInput, out enteredScore))
+        {
+            numberOfSubmittedScores++;
+            sumOfSubmittedScores += enteredScore;
+        }
+
+
+    } while (finishedEnteringScores);
+    Console.Clear();
+} while (end);

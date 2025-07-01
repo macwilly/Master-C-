82a06b47d659e28a7b678fe0252382c6db5485a8
Mackenzie Willard <Mackenzie.Willard@truetandem.com>
2025-07-01 17:07:34 -0400

    Adds a basic console application.
    Creates a simple console application that takes two numbers as input from the user and returns their sum.

---
+++ OldStyle/OldStyle/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace OldStyle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Two Numbers and I will return the sum.");
+            int userFirstNumber = int.Parse(Console.ReadLine());
+            int userSecondNumber = int.Parse(Console.ReadLine());
+            
+            int sum = AddTwoValues(userFirstNumber, userSecondNumber);
+            Console.WriteLine(sum);
+        }
+
+        static int AddTwoValues(int num1, int num2)
+        { 
+            int sum = num1 + num2;
+            return sum;
+        }
+    }
+}

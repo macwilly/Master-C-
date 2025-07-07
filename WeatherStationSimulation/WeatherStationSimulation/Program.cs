6cbfd50dd51cc51e6d9401a007c5acc3e7582aa5
Mackenzie Willard <Mackenzie.Willard@truetandem.com>
2025-07-07 19:52:06 -0400

    Simulates weather data for a given number of days
    Implements basic weather simulation by generating
    random temperature and weather conditions for a user-
    specified number of days. This provides a foundation
    for future enhancements to the weather station
    simulation.

--- WeatherStationSimulation/WeatherStationSimulation/Program.cs
+++ WeatherStationSimulation/WeatherStationSimulation/Program.cs
@@ -2,9 +2,27 @@
 {
     internal class Program
     {
+
+
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Enter the number of days to simulate:");
+            int days = int.Parse(Console.ReadLine());
+
+            int[] temperature = new int[days];
+            string[] conditions = { "Sunny", "Rainy", "Cloudy", "Snowy"};
+            string[] weatherConditions = new string[days];
+
+            Random random = new Random();
+
+            for (int i = 0; i < days; i++)
+            {
+                temperature[i] = random.Next(0, 101);
+                weatherConditions[i] = conditions[random.Next(0, conditions.Length)];
+            }
+
         }
     }
 }

74a07c01313e58e49eb26cf7b785e89487a489b1
Mackenzie Willard <Mackenzie.Willard@truetandem.com>
2025-07-07 20:05:45 -0400

    Calculates and displays average temperature.
    Adds functionality to calculate and display the average
    temperature based on the generated daily temperatures.
    This provides a more comprehensive overview of the
    simulated weather data.

--- WeatherStationSimulation/WeatherStationSimulation/Program.cs
+++ WeatherStationSimulation/WeatherStationSimulation/Program.cs
@@ -14,14 +14,37 @@
             int[] temperature = new int[days];
             string[] conditions = { "Sunny", "Rainy", "Cloudy", "Snowy"};
             string[] weatherConditions = new string[days];
+            double averageTempature;
 
             Random random = new Random();
 
             for (int i = 0; i < days; i++)
             {
                 temperature[i] = random.Next(0, 101);
-                weatherConditions[i] = conditions[random.Next(0, conditions.Length)];
+                weatherConditions[i] = conditions[random.Next(conditions.Length)];
             }
+
+            averageTempature = AverageTempature(temperature);
+
+            foreach (int temp in temperature)
+            {
+                Console.WriteLine(temp);
+            }
+            Console.WriteLine($"The average temperature is {averageTempature}");
+            Console.ReadKey();
+
+        }
+
+        static double AverageTempature(int[] temp)
+        { 
+            double sum = 0;
+            double average = 0;
+            foreach (int i in temp)
+            {
+                sum += i;
+            }
+            average = sum / temp.Length;
+            return average;
 
         }
     }

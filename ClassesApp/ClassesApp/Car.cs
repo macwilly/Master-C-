ea58956400643a1276c604cc52d0708ee4b46362
Mackenzie Willard <Mackenzie.Willard@truetandem.com>
2025-07-09 17:07:45 -0400

    Adds initial Car class
    Introduces a basic Car class to the ClassesApp project.
    This provides a foundation for future car-related functionalities.

---
+++ ClassesApp/ClassesApp/Car.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesApp
+{
+    internal class Car
+    {
+    }
+}

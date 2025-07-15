5a4aeeeecfbb0d53cf023188aff05c9447dc41f6
Mackenzie Willard <Mackenzie.Willard@truetandem.com>
2025-07-14 20:18:15 -0400

    Adds customer class and basic program flow
    Introduces a Customer class with constructors and properties.
    Also includes basic program flow to demonstrate the class.

---
+++ ClassesApp/ClassesApp/Customer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesApp
+{
+    internal class Customer
+    {
+
+
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string ContactNumber { get; set; }
+
+        //Custom Constructor
+
+        public Customer(string name, string address, string contactNumber)
+        {
+            Name = name;
+            Address = address;
+            ContactNumber = contactNumber;
+        }
+
+        public Customer(string name)
+        {
+            Name = name;
+        }
+
+        //default cunstructor
+        public Customer() {
+            Name = "New Customer";
+            Address = "No Address";
+            ContactNumber = "No Number";
+        }
+    }
+}
--- ClassesApp/ClassesApp/Program.cs
+++ ClassesApp/ClassesApp/Program.cs
@@ -4,12 +4,26 @@
     {
         static void Main(string[] args)
         {
-            Car audi = new Car("A3", "BMW", false);
-            Car bmw = new Car("i7", "Audi", true);
+            //Car audi = new Car("A3", "BMW", false);
+            //Car bmw = new Car("i7", "Audi", true);
 
-            audi.Brand = Console.ReadLine();
+            //audi.Brand = Console.ReadLine();
 
-            Console.WriteLine(audi.AboutCar());
+            //Console.WriteLine(audi.AboutCar());
+
+            Customer customer1 = new Customer("David");
+            Customer customer2 = new Customer("Frank the Tank", "1 Main ST", "13-858-7895");
+            Customer customer3 = new Customer();
+            Console.WriteLine("Name of Customer " + customer1.Name);
+            Console.WriteLine("Name of Customer " + customer2.Name);
+            Console.WriteLine("Name of Customer " + customer3.Name);
+            
+
+            Console.WriteLine("Please enter customer name.");
+
+            customer3.Name = Console.ReadLine();
+            Console.WriteLine("Name of Customer " + customer3.Name);
+
 
             Console.ReadKey();
         }
